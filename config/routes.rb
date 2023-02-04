26b8241f95f124d93505227687fef1b34cde0e64
Pete Hawkins <petey@hey.com>
2023-02-04 12:53:11 +0000

    Disable routes for custom controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :signups, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  # resources :signups, only: [:new, :create]
+  # resources :sessions, only: [:new, :create, :destroy]
 
   root "members#index"
 end
