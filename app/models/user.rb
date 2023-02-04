a891d1279baa90ee6b7230fb4d043e7d14e8d430
Pete Hawkins <petey@hey.com>
2023-02-04 12:59:26 +0000

    Add commented out methods for has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :display_name, presence: true
+
+  # has_secure_password
+
+  # Support Devise DB column name
+  # def password_digest
+  #   encrypted_password
+  # end
+
+  # def password_digest=(value)
+  #   self.encrypted_password = value
+  # end
 end
